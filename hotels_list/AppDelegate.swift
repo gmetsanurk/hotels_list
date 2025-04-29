57517353600734660f3690bf12d76309ce02cca3
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-04-29 11:18:43 +0300

    Initial commit

---
+++ hotels_list/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  hotels_list
+//
+//  Created by Georgy on 2025-04-29.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        setupWindow()
+        return true
+    }
+    
+    private func setupWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeVC = ViewController()
+        window?.rootViewController = homeVC
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ hotels_list/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hotels_list
+//
+//  Created by Georgy on 2025-04-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+
+}
+
