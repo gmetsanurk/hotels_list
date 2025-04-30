09a0e5af01555ebead2447310a4e383b92d7157c
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-04-30 20:04:30 +0300

    Starting code NetworkManager

---
+++ hotels_list/NetworkManager.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkManager.swift
+//  hotels_list
+//
+//  Created by Georgy on 2025-04-30.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidUrl
+    case invalidResponse
+    case decodingError
+    case invalidImageData
+}
+
+final class NetworkManager {
+    
+}
