1088c9de3497492745d5015f33fe9c12085921b1
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-04-30 11:20:05 +0300

    Code for Hotels model

---
+++ hotels_list/Models/Hotels.swift
@@ -0,0 +1,17 @@
+//
+//  Hotels.swift
+//  hotels_list
+//
+//  Created by Georgy on 2025-04-30.
+//
+
+import Foundation
+
+struct Hotels {
+    let id: UUID
+    let name: String
+    let address: String
+    let stars: Double
+    let distance: Double
+    let suitesAvailability: String
+}
