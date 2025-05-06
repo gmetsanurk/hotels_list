5ac57022632fd4738cbc0d16e6f5f96306c368dd
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-05-06 16:59:33 +0300

    configure HotelCell

--- hotels_list/HotelsList/Views/HotelCell.swift
+++ hotels_list/HotelsList/Views/HotelCell.swift
@@ -20,4 +20,19 @@ class HotelCell: UICollectionViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func configure(with summary: HotelSummary) {
+        nameLabel.text = summary.name
+        if let dist = summary.distance {
+            distanceLabel.text = String("Distance: \(dist) km")
+        } else {
+            distanceLabel.text = nil
+        }
+        if let suitesStr = summary.suitesAvailability,
+           let count = Int(suitesStr) {
+            availabilityLabel.text = "Available: \(count)"
+        } else {
+            availabilityLabel.text = nil
+        }
+    }
 }
