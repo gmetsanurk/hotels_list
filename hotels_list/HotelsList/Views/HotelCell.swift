8715029dd7966d5011457617dbb2a763efeb41fd
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-05-06 16:53:16 +0300

    HotelCell init

--- hotels_list/HotelsList/Views/HotelCell.swift
+++ hotels_list/HotelsList/Views/HotelCell.swift
@@ -8,5 +8,16 @@
 import UIKit
 
 class HotelCell: UICollectionViewCell {
+    static let reuseID = "HotelCell"
+    private let nameLabel = UILabel()
+    private let distanceLabel = UILabel()
+    private let availabilityLabel = UILabel()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
