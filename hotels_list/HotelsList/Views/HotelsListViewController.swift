a35d8e7c549698bc0c701a73a468b28ae8a9589e
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-05-07 11:17:41 +0300

    Some constraints

--- hotels_list/HotelsList/Views/HotelsListViewController.swift
+++ hotels_list/HotelsList/Views/HotelsListViewController.swift
@@ -13,9 +13,9 @@ extension DependencyValues {
         get { self[DataSourceKey.self] }
         set { self[DataSourceKey.self] = newValue }
     }
-
+    
     private enum DataSourceKey: DependencyKey {
-      static let liveValue: any DataSource = iMofasNetworkManager()
+        static let liveValue: any DataSource = iMofasNetworkManager()
     }
 }
 
@@ -24,13 +24,14 @@ extension DependencyValues {
         get { self[LocalDataSourceKey.self] }
         set { self[LocalDataSourceKey.self] = newValue }
     }
-
+    
     private enum LocalDataSourceKey: DependencyKey {
-      static let liveValue: any LocalDataSource = SomeLocal()
+        static let liveValue: any LocalDataSource = SomeLocal()
     }
 }
 
 let coordinator = Coordinator()
+let collectionViewMagicNumber: CGFloat = 16
 
 //MARK: - ViewController
 
@@ -75,6 +76,24 @@ extension HotelsListViewController {
             // self?.tableView.reloadData()
             print(self?.store.state.hotels)
         }
+    }
+    
+    private func setupSubviews() {
+        view.addSubview(hotelListCollectionView)
+        hotelListCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hotelListCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: collectionViewMagicNumber),
+            hotelListCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: collectionViewMagicNumber),
+            hotelListCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -collectionViewMagicNumber),
+            hotelListCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -collectionViewMagicNumber)
+        ])
+
+        view.addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
 
